b0d578680e364eed86ba68ec03e679a6d9acb5cb
DSwider <75119651+DSwider@users.noreply.github.com>
2024-01-01 20:25:31 +0100

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -27,23 +27,6 @@ var x = setInterval(function() {
   }
 }, 1000);
 
-
-(function(){
-  
-  // Defaults
-  var BUTTON = document.getElementById('btnpopup');
-  var POPUP = document.getElementById('mypopup');
-  var POPUP_CLASS = 'popup-ui';
-  
-  // Popup button (Click on button to open modal)
-  BUTTON.addEventListener('click', function(){
-    POPUP.className = POPUP_CLASS + ' show';
-  }, false);
-
-  // Popup modal (Click on modal to close it)
-  POPUP.addEventListener('click', function(){
-    this.className = POPUP_CLASS;
-  }, false);
-  
-  
-})();
+function togglePopup(){
+  document.getElementById("popup-1").classList.toggle("active");
+}
